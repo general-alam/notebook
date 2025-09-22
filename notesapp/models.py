9e6a7d64edd778ca83e4bd445bc44d17fb890838
general-alam <145622268+general-alam@users.noreply.github.com>
2025-09-22 10:07:02 +0100

    Add slug save() to models

--- notesapp/models.py
+++ notesapp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Note(models.Model):
 
     CATEGORY = (('WORK','Work'),
@@ -12,6 +10,18 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     slug = models.SlugField(unique = True)
-    category = models.CharField(max_length = 15)
+    category = models.CharField(max_length = 15, choices=CATEGORY, default="PERSONAL")
     created = models.DatesTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now = True)
+
+    def save(self, *args, **kwargs):
+        """
+        Generate a unique slug from the title on first save.
+        """
+        if not self.slug:
+            slug_base = slugify(self.title)
+            slug = slug_base
+            if Note.objects.filter(slug=slug).exists():
+                slug = f'{slug_base}-{get_random_string(5)}'
+            self.slug = slug
+        super(Note, self).save(*args, **kwargs)
